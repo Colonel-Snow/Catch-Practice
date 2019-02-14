55a20d424f48df4f93308096783d84a7d5236120
unknown <kernellsnow@gmail.com>
2019-02-13 16:08:50 -0900

    added base files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kerne on 2/13/2019.
+//
+
+#include "catch.hpp"
+#include "tests.cpp"
+
+int main {
+    return 0;
+};
+
---
+++ tests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kerne on 2/13/2019.
+//
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+unsigned int stringCalc( string c ) {
+    if(c == "") return 0;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( stringCalc("") == 0 );
+}
